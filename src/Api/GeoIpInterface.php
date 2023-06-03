142cffb387132a08e505a5c74298aab877958e43
Burial0268 <burial0268@foxmail.com>
2023-06-03 23:21:37 +0800

    chore: Just an interface

---
+++ src/Api/GeoIpInterface.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * This file is part of GBCLStudio Project.
+ *
+ * Copyright (c) 2023 GBCLStudio PHP Project Team.
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+namespace GBCLStudio\GeoIp\Api;
+
+use GBCLStudio\GeoIp\ServiceResponse;
+
+interface GeoIpInterface
+{
+    public function get(string $ip): ServiceResponse;
+}
