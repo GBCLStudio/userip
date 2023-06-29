2d867bbce0df78913d99a4f35bbca49b660a2304
Burial0268 <burial0268@foxmail.com>
2023-06-30 01:11:32 +0800

    refactor(front): why TSX

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -12,7 +12,6 @@ import {extend} from 'flarum/common/extend';
 import CommentPost from 'flarum/forum/components/CommentPost';
 import ipinfo from './Model/ipinfo';
 import ProcessData from "./ProcessData";
-import UserIpToolBar from "./components/UserIpToolBar";
 import Model from 'flarum/common/Model';
 
 const getData = (ipInfo) => {
@@ -29,7 +28,7 @@ app.initializers.add('gbcl/userip', () => {
     const errorNotice = app.translator.trans("gbcl-userip.forum.unknownNotice");
 
     app.store.models.userip_info = ipinfo;
-    app.store.models.Posts.prototype.ipInfo = Model.hasOne('userip_info');
+    app.store.models.posts.prototype.ipInfo = Model.hasOne('userip_info');
 
     extend(CommentPost.prototype, 'footerItems', function (items) {
 
@@ -40,6 +39,7 @@ app.initializers.add('gbcl/userip', () => {
         const {region, countryCode, isp} = getData(ipInfo);
 
         const result = new ProcessData(region, countryCode, isp).process(errorNotice)
+        console.log(result);
 
         const [reg, code, serv] = result.elements;
         const errorCount = result.count;
@@ -47,7 +47,11 @@ app.initializers.add('gbcl/userip', () => {
         if (errorCount < 2) {
             items.add(
                 'userIp',
-                <UserIpToolBar region={reg} countryCode={code} isp={serv}/>
+                <div className="userIp-container">
+                    <div className="ip-locate" id="info">
+                        {`${reg}, ${code} | ${serv}`}
+                    </div>
+                </div>
             );
         }
     })
