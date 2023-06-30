eafc1a7b7dfd1c0aa6e0b045fabc8858eed948f9
Burial0268 <burial0268@foxmail.com>
2023-06-30 15:51:49 +0800

    chore(cleanup)
    No longer conflicts with fof/geoip // remove unused code // TSX

---
+++ js/src/forum/components/GeoIpToolBar.tsx
@@ -0,0 +1,29 @@
+/*
+ * This file is part of GBCLStudio Project.
+ *
+ * Copyright (c) 2023 GBCLStudio PHP Project Team.
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+import Component, { ComponentAttrs } from "flarum/common/Component";
+import type Mithril from 'mithril';
+
+export interface GeoIpBarAttrs extends ComponentAttrs {
+    code: string;
+    region: string;
+    isp: string;
+}
+export default class GeoIpToolBar<CustomAttrs extends GeoIpBarAttrs = GeoIpBarAttrs> extends Component<CustomAttrs> {
+    view(vnode: Mithril.VnodeDOM<CustomAttrs, this>) {
+        let { code, region, isp } = this.attrs;
+        return (
+            <div className="userIp-container">
+                <div className="ip-locate" id="info">
+                    {`${region}, ${code} | ${isp}`}
+                </div>
+            </div>
+        )
+    }
+}
--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -8,11 +8,12 @@
  */
 
 import app from 'flarum/forum/app';
-import {extend} from 'flarum/common/extend';
+import { extend } from "flarum/common/extend";
 import CommentPost from 'flarum/forum/components/CommentPost';
 import ipinfo from './Model/ipinfo';
 import ProcessData from "./ProcessData";
 import Model from 'flarum/common/Model';
+import GeoIpToolBar from "./components/GeoIpToolBar";
 
 const getData = (ipInfo) => {
     return {
@@ -28,18 +29,17 @@ app.initializers.add('gbcl/userip', () => {
     const errorNotice = app.translator.trans("gbcl-userip.forum.unknownNotice");
 
     app.store.models.userip_info = ipinfo;
-    app.store.models.posts.prototype.ipInfo = Model.hasOne('userip_info');
+    app.store.models.posts.prototype.userIpInfo = Model.hasOne('userip_info');
 
     extend(CommentPost.prototype, 'footerItems', function (items) {
 
-        const ipInfo = this.attrs.post.ipInfo();
+        const ipInfo = this.attrs.post.userIpInfo();
 
         if (!ipInfo) return;
 
         const {region, countryCode, isp} = getData(ipInfo);
 
         const result = new ProcessData(region, countryCode, isp).process(errorNotice)
-        console.log(result);
 
         const [reg, code, serv] = result.elements;
         const errorCount = result.count;
@@ -47,12 +47,8 @@ app.initializers.add('gbcl/userip', () => {
         if (errorCount < 2) {
             items.add(
                 'userIp',
-                <div className="userIp-container">
-                    <div className="ip-locate" id="info">
-                        {`${reg}, ${code} | ${serv}`}
-                    </div>
-                </div>
-            );
+                <GeoIpToolBar region={reg} code={code} isp={serv}/>
+            )
         }
     })
 })
